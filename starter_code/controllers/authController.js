a819f289801e2c2b33bb5f306b3a713514d6aae6
Paula Quitián <pquitian@idealista.com>
2018-07-21 13:36:55 +0200

    feat: error message is user exists

--- starter_code/controllers/authController.js
+++ starter_code/controllers/authController.js
@@ -1,9 +1,28 @@
 module.exports.create = (req, res, next) => {
-    res.render('auth/signup',{
-        errorMessage:""
-    });
+    res.render('auth/signup');
 }
 
 module.exports.doCreate =  (req, res, next) =>{
- 
+    const nameInput = req.body.name; 
+    const emailInput = req.body.email;
+    const passwordInput = req.body.password;
+
+    if (emailInput === '' || passwordInput === '') {
+        res.render('auth/signup', {
+            errorMessage: "Enter both email and password"
+        });
+    } else {
+        User.findOne({email: emailInput})
+            .then(user => {
+                if (user) {
+                    res.render('auth/signup', {
+                        user: req.body,
+                        errors: { email: 'Email already registered' }
+                    });
+                } 
+            })
+            .catch()
+            
+    }
+        
 }
